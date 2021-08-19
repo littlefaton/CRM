45b53f0ae70dbeed15292ec4cd674440c61acd10
littlefaton <cylam296-c@my.cityu.edu.hk>
2021-08-19 15:33:33 +0800

    Add Dashboard userinfo

--- src/components/_dashboard/app/AppClientInfo.js
+++ src/components/_dashboard/app/AppClientInfo.js
@@ -1,23 +1,13 @@
-import { Icon } from '@iconify/react';
-import androidFilled from '@iconify/icons-ant-design/android-filled';
 // material
-import { alpha, styled } from '@material-ui/core/styles';
+import { styled } from '@material-ui/core/styles';
 import {
   Card,
   Table,
-  Stack,
-  Avatar,
   TableRow,
-  TableHead,
-  TableBody,
   TableCell,
-  Container,
-  Typography,
-  TableContainer,
-  TablePagination
-} from '@material-ui/core';
+  Typography} from '@material-ui/core';
 // mocks
-import userInfo from '../_mocks_/userInfo.json';
+import AppUserInfo from 'src/_mocks_/AppUserInfo.json';
 
 // ----------------------------------------------------------------------
 
@@ -32,51 +22,20 @@ const RootStyle = styled(Card)(({ theme }) => ({
 
 // ----------------------------------------------------------------------
 
-const TOTAL = 714000;
-
-const TABLE_HEAD = [
-  { id: 'name', label: 'Name', alignRight: false },
-  { id: 'gender', label: 'Gender', alignRight: false },
-  { id: 'age', label: 'Age', alignRight: false },
-  { id: 'nationality', label: 'Nationality', alignRight: false },
-  { id: 'contact', label: 'Contact', alignRight: false },
-  { id: 'address', label: 'Address', alignRight: false }
-];
-
 export default function AppClientInfo() {
-  // {USERINFO.map(props => {
   return (
     <RootStyle>
 
       <Typography variant="h5" align="left" paddingLeft={2}>Client Info</Typography>
-      <Table size="small" padding="checkbox">
+      {AppUserInfo.map((item, i) => (
+      <Table size="small" padding="checkbox" key="i" >
     <TableRow>
-        <TableCell variant="head">Name</TableCell>
-        <TableCell>Cell 1</TableCell>
-    </TableRow>
-    <TableRow>
-        <TableCell variant="head">Gender</TableCell>
-        <TableCell>Cell 2</TableCell>
-    </TableRow>
-    <TableRow>
-        <TableCell variant="head">Age</TableCell>
-        <TableCell>Cell 2</TableCell>
-    </TableRow>
-    <TableRow>
-        <TableCell variant="head">Nationality</TableCell>
-        <TableCell>Cell 2</TableCell>
-    </TableRow>
-    <TableRow>
-        <TableCell variant="head">Contact</TableCell>
-        <TableCell>Cell 2</TableCell>
-    </TableRow>
-    <TableRow>
-        <TableCell variant="head">Address</TableCell>
-        <TableCell>Cell 2</TableCell>
+        <TableCell variant="head">{item.header}</TableCell>
+        <TableCell align="right">{item.content}</TableCell>
     </TableRow>
 </Table>
+    ))}
 
     </RootStyle>
   );
-//})}
 }
