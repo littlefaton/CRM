df7d846f98481031b0bc944f57c2e24f7148196f
littlefaton <cylam296-c@my.cityu.edu.hk>
2021-08-26 18:01:06 +0800

    Delete blog and product from routes

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,6 @@ import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import DashboardApp from './pages/DashboardApp';
-import Products from './pages/Products';
-import Blog from './pages/Blog';
 import User from './pages/User';
 import NotFound from './pages/Page404';
 
@@ -22,8 +20,6 @@ export default function Router() {
         { path: '/', element: <Navigate to="/dashboard/user" replace /> },
         { path: 'app', element: <DashboardApp /> },
         { path: 'user', element: <User /> },
-        { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> },
         { path: 'app/:id', element: <DashboardApp /> },
       ]
     },
