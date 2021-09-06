d1f7dab1f76f7b4d5ca72482c2e62644dd97db51
cyleung428 <msf96a85@gmail.com>
2021-09-06 16:17:22 +0800

    Add Table to customer holding with search bar

--- src/pages/CustomerHolding.js
+++ src/pages/CustomerHolding.js
@@ -10,7 +10,32 @@ import { fNumber } from '../utils/formatNumber';
 import { BaseOptionChart } from '../components/charts';
 import Page from '../components/Page';
 //mocks
-import HOLDINGDATA from 'src/_mocks_/holdingData.json'
+import HOLDINGDATA from 'src/_mocks_/holdingData.json';
+
+//-----------------------------------------------------------
+
+import { filter } from 'lodash';
+import { sentenceCase } from 'change-case';
+import { useState } from 'react';
+import React from 'react';
+// material
+import {
+  Card,
+  Table,
+  Avatar,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TablePagination
+} from '@material-ui/core';
+// components
+import Label from '../components/Label';
+import Scrollbar from '../components/Scrollbar';
+import SearchNotFound from '../components/SearchNotFound';
+import { UserListHead, UserListToolbar } from '../components/_dashboard/user';
+//
+import USERLIST from '../_mocks_/user.json';
 
 // ----------------------------------------------------------------------
 
@@ -54,7 +79,87 @@ const useStyles = makeStyles({
 
 const CHART_DATA = [4344, 5435, 1443, 4443];
 
-export default function Cash() {
+//----------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  { id: 'market', label: 'Market', alignRight: false },
+  { id: 'code', label: 'Instrument Code', alignRight: false },
+  { id: 'name', label: 'Instrument Name', alignRight: false },
+  { id: 'chinName', label: 'Instrument Chinese Name', alignRight: false },
+  { id: 'cost', label: 'Cost', alignRight: false },
+  { id: 'quantity', label: 'Quantity', alignRight: false },
+  { id: 'price', label: 'Market Price', alignRight: false }
+];
+
+// ----------------------------------------------------------------------
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function applySortFilter(array, comparator, query) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  return stabilizedThis.map((el) => el[0]);
+}
+
+export default function CustomerHolding() {
+
+  const [page, setPage] = useState(0);
+  const [order, setOrder] = useState('asc');
+  const [selected, setSelected] = useState([]);
+  const [orderBy, setOrderBy] = useState('name');
+  const [filterName, setFilterName] = useState('');
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleFilterByName = (event) => {
+    setFilterName(event.target.value);
+  };
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - HOLDINGDATA.length) : 0;
+
+  const filteredUsers = applySortFilter(HOLDINGDATA, getComparator(order, orderBy), filterName);
+
+  const isUserNotFound = filteredUsers.length === 0;
+
+
   const theme = useTheme();
   const classes = useStyles();
   const bull = <span className={classes.bullet}>•</span>;
@@ -97,7 +202,78 @@ export default function Cash() {
           <ReactApexChart type="pie" series={CHART_DATA} options={chartOptions} height={350}/>
         </ChartWrapperStyle>
 
-        
+        <Card>
+          <UserListToolbar
+            numSelected={selected.length}
+            filterName={filterName}
+            onFilterName={handleFilterByName}
+          />
+
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <UserListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={HOLDINGDATA.length}
+                  onRequestSort={handleRequestSort}
+                />
+                <TableBody>
+                  {filteredUsers
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
+                      const { id, market, code, name, chinName, cost, quantity, price } = row;
+                      const isItemSelected = selected.indexOf(name) !== -1;
+
+                      return (
+                        <TableRow
+                          hover
+                          key={id}
+                          tabIndex={-1}
+                          alignItems= "center"
+                          style={{ textDecoration: 'none' }}
+                        >
+                          <TableCell align="left">{market}</TableCell>
+                          <TableCell align="left">{code}</TableCell>
+                          <TableCell align="left">{name}</TableCell>
+                          <TableCell align="left">{chinName}</TableCell>
+                          <TableCell align="left">{cost}</TableCell>
+                          <TableCell align="left">{quantity}</TableCell>
+                          <TableCell align="left">{price}</TableCell>
+
+                        </TableRow>
+                      );
+                    })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                {isUserNotFound && (
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        <SearchNotFound searchQuery={filterName} />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={HOLDINGDATA.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card>
 
     </Container>
     </Page>
